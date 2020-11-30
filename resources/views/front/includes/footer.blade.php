df3c1f1bd1d7b67828d48ceafd141d14de654290
MaxChernyshev <maksim.chernyshev@gmail.com>
2020-11-30 19:49:38 +0200

    main page + layouts

---
+++ resources/views/front/includes/footer.blade.php
@@ -0,0 +1,65 @@
+<footer>
+    <div class="container-fluid mt-5">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
+                <nav class="navbar navbar-expand-lg">
+                    <a class="navbar-brand" href="#">
+                        <img class="logobg" src="{{asset('assets/front/img/logo.jpg')}}"></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </nav>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                <div>
+                    <i class="fa fa-phone" aria-hidden="true">+38 (067) 320-30-11</i>
+                </div>
+                <div>
+                    <i class="fa fa-envelope" aria-hidden="true">danyliukmikhailo@gmail.com</i>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="container-fluid copyright_container">
+        <div class="row">
+            <div class="col-12">
+                <div class="copyright">Agriсo group © {{date('Y')}} All Rights Reserved.</div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="scrollup">
+        <!-- Иконка fa-chevron-up (Font Awesome) -->
+        <i class="fa fa-chevron-up"></i>
+    </div>
+</footer>
+
+
+
+<script src="{{asset('assets/front/js/scripts.min.js')}}"></script>
+
+
---
+++ resources/views/front/includes/header.blade.php
@@ -0,0 +1,42 @@
+<header class="header fixed-top" id="header">
+    <div class="container-fluid p-0 m-0 ">
+        <div class="row p-0 m-0">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 p-0 m-0">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand" href="#">
+                        <img class="logobg" src="{{asset('assets/front/img/logo.jpg')}}"></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                        </ul>
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="form-control mr-sm-2 search" type="search" placeholder="Пошук"
+                                   aria-label="Search">
+                            <button class="btn btn-outline-success my-2 my-sm-0 searchbutton" type="submit">
+                                Пошук
+                            </button>
+                        </form>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+
